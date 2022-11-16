3e5055ee1602b3e06dab986e68c074cf4af10862
Leon <nine_iota1@proton.me>
2022-11-16 16:17:51 +0100

    Setup project

--- pwmanager.sh
+++ pwmanager.sh
@@ -1,1 +1,34 @@
 #!/usr/bin/env bash
+#
+# @author     Leon Kiefer
+# @date       16.11.2022
+
+function help {
+  echo "Hello World"
+}
+
+function get_pw {
+  echo "Hello World"
+}
+
+function decrypt_pw_db {
+  echo "Hello World"
+}
+
+function save_pw {
+  echo "Hello World"
+}
+
+function delete_pw {
+  echo "Hello World"
+}
+
+help
+
+get_pw
+
+decrypt_pw_db
+
+save_pw
+
+delete_pw
