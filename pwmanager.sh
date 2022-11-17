ff600f20be75f2dbbed71a449248366785922783
Leon <nine_iota1@proton.me>
2022-11-17 21:30:32 +0100

    Add variables

--- pwmanager.sh
+++ pwmanager.sh
@@ -3,6 +3,20 @@
 # @author     Leon Kiefer
 # @date       16.11.2022
 
+user_home=$(eval echo ~$USERNAME)
+
+pw_db="$user_home/.pwmanager_db"
+
+pubkey="$user_home/.pwmanager_pubkey"
+
+crud="create"
+
+pw_length=69
+
+pw=
+
+id=
+
 function help {
   echo "Hello World"
 }
