ffddad6be1dfc75f50fbc222ecc9e7ebb493b4fd
Leon <nine_iota1@proton.me>
2022-11-18 22:52:54 +0100

    Update installer script

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # @author     Leon Kiefer
-# @date       16.11.2022#
+# @date       16.11.2022
 
 read -p "Do you want to update your packages before proceeding? [Y/n] " -r res
 
@@ -19,10 +19,3 @@ if [ "$(dpkg-query -W -f='${Status}' xclip 2>/dev/null | grep -c "ok installed")
   echo 'xclip not installed .... installing now!'
   sudo apt install xclip -y
 fi
-
-echo "adding alias to ~/.bashrc"
-echo "# pbcopy & pbpaste alias" >> ~/.bashrc
-echo "alias pbcopy='xclip -sel clip'" >> ~/.bashrc
-echo "alias pbpaste='xclip -sel clip -o'" >> ~/.bashrc
-
-source ~/.bashrc
